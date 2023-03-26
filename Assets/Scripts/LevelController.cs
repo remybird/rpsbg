5a42f5c0c0fee6873ce2e55a640ef004bafaf3ab
Michael Bayne <mdb@samskivert.com>
2023-03-26 12:23:03 -0700

    Add a border around the grass.

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -4,6 +4,7 @@ using UnityEngine.Tilemaps;
 public class LevelController : MonoBehaviour {
 
   public Tile grass;
+  public Tile barrier;
   public Tilemap floor;
   public Tilemap walls;
 
@@ -15,6 +16,11 @@ public class LevelController : MonoBehaviour {
     floor.ClearAllTiles();
     for (var yy = -20; yy < 20; yy += 1) for (var xx = -20; xx < 20; xx += 1) {
       floor.SetTile(new Vector3Int(xx, yy, 0), grass);
+    }
+    walls.ClearAllTiles();
+    for (var yy = -30; yy < 30; yy += 1) for (var xx = -30; xx < 30; xx += 1) {
+      if (yy >= -20 && yy < 20 && xx >= -20 && xx < 20) continue;
+      walls.SetTile(new Vector3Int(xx, yy, 0), barrier);
     }
   }
 }
