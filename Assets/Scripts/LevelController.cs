db6f42a99e344f0b420c7879bc3f0623c6053041
ninetyninecats <catticus.bayne@gmail.com>
2023-03-26 13:49:08 -0700

    Mountains

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -5,6 +5,8 @@ public class LevelController : MonoBehaviour {
 
   public Tile grass;
   public Tile barrier;
+  public Tile[] mountainB;
+  public Tile[] mountainS;
   public Tilemap floor;
   public Tilemap walls;
 
@@ -22,5 +24,18 @@ public class LevelController : MonoBehaviour {
       if (yy >= -20 && yy < 20 && xx >= -20 && xx < 20) continue;
       walls.SetTile(new Vector3Int(xx, yy, 0), barrier);
     }
+    for (var xx = -20; xx < 20; xx += 3) {
+      SetMountainB(xx, 21);
+      SetMountainB(xx, -21);
+    }
+  }
+  
+  private void SetMountainB (int xx, int yy) {
+    walls.SetTile(new Vector3Int(xx, yy, 0), mountainB[0]);
+    walls.SetTile(new Vector3Int(xx + 1, yy, 0), mountainB[1]);
+    walls.SetTile(new Vector3Int(xx + 2, yy, 0), mountainB[2]);
+    walls.SetTile(new Vector3Int(xx, yy - 1, 0), mountainB[3]);
+    walls.SetTile(new Vector3Int(xx + 1, yy - 1, 0), mountainB[4]);
+    walls.SetTile(new Vector3Int(xx + 2, yy - 1, 0), mountainB[5]);
   }
 }
