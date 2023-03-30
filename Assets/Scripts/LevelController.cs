fc4ae7bad1efa50f33f6ea3c9218ba34a38cf52b
Michael Bayne <mdb@samskivert.com>
2023-03-29 18:20:00 -0700

    Slightly less jamky mountains.

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -16,11 +16,11 @@ public class LevelController : MonoBehaviour {
 
   private void GenerateLevel () {
     floor.ClearAllTiles();
-    for (var yy = -21; yy < 20; yy += 1) for (var xx = -21; xx < 21; xx += 1) {
+    for (var yy = -21; yy < 20; yy += 1) for (var xx = -21; xx < 22; xx += 1) {
       floor.SetTile(new Vector3Int(xx, yy, 0), grass);
     }
     for (var yy = -30; yy < 30; yy += 1) for (var xx = -30; xx < 30; xx += 1) {
-      if (yy >= -21 && yy < 20 && xx >= -21 && xx < 21) continue;
+      if (yy >= -21 && yy < 20 && xx >= -21 && xx < 22) continue;
       floor.SetTile(new Vector3Int(xx, yy, 0), barrier);
     }
     walls.ClearAllTiles();
@@ -28,10 +28,15 @@ public class LevelController : MonoBehaviour {
       SetMountainB(xx, 21);
       SetMountainB(xx, -20);
     }
-    for (var yy = -19; yy < 20; yy += 2) {
-      SetMountainS(-22, yy);
-      SetMountainS(20, yy);
+
+    SetTiles(-22, -21, mountainS[4], mountainS[5]);
+    SetTiles(21, -21, mountainS[4], mountainS[5]);
+    for (var yy = -20; yy < 20; yy += 1) {
+      SetTiles(-22, yy, mountainS[2], mountainS[3]);
+      SetTiles(21, yy, mountainS[2], mountainS[3]);
     }
+    SetTiles(-22, 20, mountainS[0], mountainS[1]);
+    // SetTiles(21, 20, mountainS[2], mountainS[3]);
   }
 
   private void SetMountainB (int xx, int yy) {
@@ -43,10 +48,8 @@ public class LevelController : MonoBehaviour {
     walls.SetTile(new Vector3Int(xx + 2, yy - 1, 0), mountainB[5]);
   }
 
-  private void SetMountainS (int xx, int yy) {
-    walls.SetTile(new Vector3Int(xx, yy, 0), mountainS[0]);
-    walls.SetTile(new Vector3Int(xx + 1, yy, 0), mountainS[1]);
-    walls.SetTile(new Vector3Int(xx, yy - 1, 0), mountainS[2]);
-    walls.SetTile(new Vector3Int(xx + 1, yy - 1, 0), mountainS[3]);
+  private void SetTiles (int xx, int yy, Tile left, Tile right) {
+    walls.SetTile(new Vector3Int(xx, yy, 0), left);
+    walls.SetTile(new Vector3Int(xx + 1, yy, 0), right);
   }
 }
